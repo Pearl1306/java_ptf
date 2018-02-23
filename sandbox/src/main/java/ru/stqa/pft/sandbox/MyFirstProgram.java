2a2386866475fb452a57c5cf4004062f1044fa58
Natalya <linatali1306@gmail.com>
2018-02-23 04:02:49 -0500

    rrrr

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,9 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+        hello ("world");
+        hello("user");
+        hello("Natali");
 
-      System.out.println("Hallo, world!");
+        double l=5;
+        System.out.println("Ploshad kvadrata so storonoy " + l +"="+ area (l));
+        double a=4;
+        double b=6;
+        System.out.println("Ploshad praymougolnika so storonami "+a+" i "+b+"=" +area( a, b));
 
     }
+    public static void hello (String somebody)  {
 
+        System.out.println("Hello,"+somebody+"!");
+    }
+    public static  double area(double len){
+        return len*len;
+    }
+    public static double area (double a, double b) {
+        return a*b;
+    }
 }
