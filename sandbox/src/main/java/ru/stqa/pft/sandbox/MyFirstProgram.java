b1503e1c00bd2dc01dc3764ae2b045e1ae5d0424
Natalya <linatali1306@gmail.com>
2018-02-23 20:38:43 -0500

    ispolzovanie "this"

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,22 +6,16 @@ public class MyFirstProgram {
         hello ("world");
         hello("user");
         hello("Natali");
+        Square s= new Square(5);
+        System.out.println("Ploshad kvadrata so storonoy " + s.l +"="+ s.area ());
 
-        double l=5;
-        System.out.println("Ploshad kvadrata so storonoy " + l +"="+ area (l));
-        double a=4;
-        double b=6;
-        System.out.println("Ploshad praymougolnika so storonami "+a+" i "+b+"=" +area( a, b));
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Ploshad praymougolnika so storonami "+r.a+" i "+r.b+"=" +r.area());
 
     }
     public static void hello (String somebody)  {
 
         System.out.println("Hello,"+somebody+"!");
     }
-    public static  double area(double len){
-        return len*len;
-    }
-    public static double area (double a, double b) {
-        return a*b;
-    }
+
 }
