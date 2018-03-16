3cac4d90f5ecc3ca7b5b61aae2e1e61f8a60a087
Natalya <linatali1306@gmail.com>
2018-03-16 03:49:02 -0400

    dobavila perehod na homepage

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,8 +8,9 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testsContactCreation() {
-    app.getNavigationHelper().gotoContactPage();
+    app.getNavigationHelper().gotoHomePage();
     int before = app.getHelper().getContactCount();
+    app.getNavigationHelper().gotoContactPage();
     app.getHelper().createContact(new ContactData("otvet0", "otvet1", "otvet2", "otvet3", "44 otvet4", "1234567890", "2345678901", "3456789012", "otvet5@gmail.com", "test1"));
     int after = app.getHelper().getContactCount();
     Assert.assertEquals(after, before + 1);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -8,10 +8,11 @@ public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion(){
-    app.getNavigationHelper().gotoHomePage();
+
     if(!app.getHelper().isThereAContact()){
       app.getHelper().createContact(new ContactData("otvet0", "otvet1", "otvet2", "otvet3", "44 otvet4", "1234567890", "2345678901", "3456789012", "otvet5@gmail.com", "test1"));
     }
+    app.getNavigationHelper().gotoHomePage();
     int before= app.getHelper().getContactCount();
     app.getHelper().selectContact();
     app.getHelper().deleteSelectedContact();
